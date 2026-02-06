fc974bd24264b8a48936464a5c93bdd1adfbf983
aman-verma-0 <vaman7256@gmail.com>
2026-02-06 22:45:20 +0530

    Time: 15 ms (49.92%) | Memory: 46.1 MB (89.36%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,16 +1,28 @@
 class Solution {
     public int numberOfSubstrings(String s) {
         int n=s.length();
-        Map<Character,Integer>map=new HashMap<>();
+        // Map<Character,Integer>map=new HashMap<>();
+        // int left=0,count=0;
+        // for(int right=0;right<n;right++){
+        //     char ch=s.charAt(right);
+        //     map.put(ch,map.getOrDefault(ch,0)+1);
+            // while(map.size()==3){                        //using map 
+        //         count+=(n-right);
+        //         char chl=s.charAt(left);
+        //         map.put(chl,map.get(chl)-1);
+        //         if(map.get(chl)==0) map.remove(chl);
+        //         left++;
+        //     }
+        // }
+        // return count;
+        int[]freq=new int[3];
         int left=0,count=0;
         for(int right=0;right<n;right++){
             char ch=s.charAt(right);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-            while(map.size()==3){
+            freq[ch-'a']++;
+            while(freq[0]>0 && freq[1]>0 && freq[2]>0){
                 count+=(n-right);
-                char chl=s.charAt(left);
-                map.put(chl,map.get(chl)-1);
-                if(map.get(chl)==0) map.remove(chl);
+                freq[s.charAt(left)-'a']--;
                 left++;
             }
         }
