3e120117163e7d174b498bd109d1df5ddada92a5
aman-verma-0 <vaman7256@gmail.com>
2026-02-10 23:08:36 +0530

    Time: 0 ms (100.00%) | Memory: 110.9 MB (76.97%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,24 +1,29 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        int edge=edges.length;
-        int nodes=edge+1;
-        ArrayList<ArrayList<Integer>> adjList=new ArrayList<>();
-        for(int i=0;i<=nodes;i++){ 
-            adjList.add(new ArrayList<>());
-        }
-        for(int i=0;i<edges.length;i++){
-            int from=edges[i][0];
-            int to=edges[i][1];
-            adjList.get(from).add(to);
-            adjList.get(to).add(from);
+        int first=edges[0][0];
+        int second=edges[0][1];
+        if(first==edges[1][0]) return first;
+        else if(second==edges[1][0]) return second;
+        return edges[1][1];
+        // int edge=edges.length;
+        // int nodes=edge+1;
+        // ArrayList<ArrayList<Integer>> adjList=new ArrayList<>();
+        // for(int i=0;i<=nodes;i++){ 
+        //     adjList.add(new ArrayList<>());
+        // }
+        // for(int i=0;i<edges.length;i++){
+        //     int from=edges[i][0];
+        //     int to=edges[i][1];
+        //     adjList.get(from).add(to);
+        //     adjList.get(to).add(from);
             
-        }
-        for(int i=0;i<=nodes;i++){
-            if(adjList.get(i).size()==edge){
-                return i;
-            }
-        }
-        return -1;
+        // }
+        // for(int i=1;i<=nodes;i++){
+        //     if(adjList.get(i).size()==edge){
+        //         return i;
+        //     }
+        // }
+        // return -1;
     }
     
 }
