904532bad8d247f1b36117ddc452cd6596219f38
aman-verma-0 <vaman7256@gmail.com>
2026-02-18 18:36:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (77.54%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n=nums.length;
+        Integer[][]dp=new Integer[n+1][n+1];
+        int result=findScore(0,n-1,nums,dp);
+
+        return result>=0;
+        
+    }
+    public int findScore(int left,int right,int[]nums,Integer[][]dp){
+        if(left==right) return nums[left];
+        if(dp[left][right]!=null) return dp[left][right];
+        
+        int lefttake=nums[left]-findScore(left+1,right,nums,dp);
+        int righttake=nums[right]-findScore(left,right-1,nums,dp);
+        return dp[left][right]=Math.max(lefttake,righttake);
+    }
+}
