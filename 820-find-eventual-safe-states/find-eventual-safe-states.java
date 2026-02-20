91bb98355862efa8a8894f971993ef98e06a847f
aman-verma-0 <vaman7256@gmail.com>
2026-02-20 11:17:47 +0530

    Time: 32 ms (14.07%) | Memory: 57.7 MB (98.81%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -7,51 +7,57 @@ class Solution {
             revList.add(new ArrayList <>());
         }
         int[]indeg=new int[n];
-        int[]outdeg=new int[n];
+        // int[]outdeg=new int[n];
         boolean[]vis=new boolean[n];
         for(int i=0;i<n;i++){
             for(int j=0;j<graph[i].length;j++){
-                outdeg[i]+=1;
+                // outdeg[i]+=1;
                 int nbr=graph[i][j];
-                indeg[nbr]+=1;
+                indeg[i]+=1;
                 revList.get(nbr).add(i);
             }
         }
         Queue<Integer>q=new LinkedList<>();
         for(int i=0;i<n;i++){
-            if(outdeg[i]==0) q.add(i);
+            if(indeg[i]==0) q.add(i);
         }
         while(!q.isEmpty()){
             int cur=q.poll();
             ans.add(cur);
             for(int nbr:revList.get(cur)){
-                outdeg[nbr]--;
-                if(outdeg[nbr]==0) q.add(nbr);
+                indeg[nbr]--;
+                if(indeg[nbr]==0) q.add(nbr);
             }
             
         }
         Collections.sort(ans);
         return ans;
         
+        // for(int i=0;i<n;i++){
+        //     if(!checkSafe(i,graph)) ans.add(i);
+
+        // }
+        // return ans;
+        
     }
-    // public boolean checkSafe(int i,int[][]graph,int[]indeg,int[]outdeg,boolean[]vis){
-    //     Queue<int[]>q=new LinkedList<>();
-    //     q.add(new int[]{i,-1});
+
+    // public boolean checkSafe(int i,int[][]graph){
+    //     Queue<Integer>q=new LinkedList<>();
+    //     boolean[]vis=new boolean[graph.length];
+    //     q.add(i);
     //     vis[i]=true;
     //     while(!q.isEmpty()){
-    //         int []curr=q.poll();
-    //         int node=curr[0];
-    //         int par=curr[1];
-    //         for(int nbr:graph[node]){
+    //         int curr=q.poll();
+    //         for(int nbr:graph[curr]){
     //             if(vis[nbr]==false){
     //                 vis[nbr]=true;
-    //                 q.add(new int[]{nbr,node});
+    //                 q.add(nbr);
     //             }
-    //             else if(par!=nbr) return true;
+    //             else return true;
     //         }
     //     }
     //     return false;
-
-
     // }
+    
+
 }
