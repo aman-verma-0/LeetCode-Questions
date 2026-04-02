115a410c6c70887434abf0be1e50ec8d122c3dd4
aman-verma-0 <vaman7256@gmail.com>
2026-04-02 14:27:51 +0530

    Time: 11 ms (32.76%) | Memory: 48.2 MB (5.75%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -2,20 +2,22 @@ class Solution {
     public int deleteAndEarn(int[] nums) {
         int n=nums.length;
         int max=10000;
-        long[]val=new long[max+1];
-        for(int X:nums){
-            val[X]+=X;
+        int[]valArr=new int[10001];
+        for(int x:nums){
+            valArr[x]+=x;
         }
-        long[]dp=new long[max+1];
+        int[]dp=new int[10001];
         Arrays.fill(dp,-1);
-        return (int)calEarning(max,val,dp);
+        return (int)findMaxPoint(max,valArr,dp);
         
     }
-    public long calEarning(int max,long[]val,long[]dp){
-        if(max<1) return 0;
-        if(dp[max]!=-1) return dp[max];
-        long pick=val[max]+calEarning(max-2,val,dp);
-        long notpick=calEarning(max-1,val,dp);
-        return dp[max]=Math.max(pick,notpick);
+    public long findMaxPoint(int points,int[]valArr,int []dp){
+        if(points<1) return 0;
+        if(dp[points]!=-1) return dp[points];
+        // long notpick=0;
+        long pick=valArr[points]+findMaxPoint(points-2,valArr,dp);
+        long notpick=findMaxPoint(points-1,valArr,dp);
+        long ans=Math.max(pick,notpick);
+        return dp[points]=(int)ans;
     }
 }
