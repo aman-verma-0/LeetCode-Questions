24e0f4267ed37e9ce10002383f88bd9fa6f72931
aman-verma-0 <vaman7256@gmail.com>
2026-04-02 15:35:27 +0530

    Time: 166 ms (14.78%) | Memory: 44.7 MB (45.65%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numSquares(int n) {
+        int[]dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return findMinNum(1,n,dp);
+        
+    }
+    public int findMinNum(int i,int n,int[]dp ){
+        if(n==0) return 0;
+        if(i*i>n) return Integer.MAX_VALUE;
+        if(dp[n]!=-1) return dp[n];
+        
+        int pick=Integer.MAX_VALUE;
+
+        if((i*i)<=n){
+            int next=findMinNum(i,n-(i*i),dp);
+            if(next!=Integer.MAX_VALUE){
+                pick=1+next;
+            }  
+        }
+        int notpick=findMinNum(i+1,n,dp);
+        return dp[n]=Math.min(pick,notpick);
+    }
+}
