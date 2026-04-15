c19fbc936c5634f72b7a94af8ed6e17e745a0aec
aman-verma-0 <vaman7256@gmail.com>
2026-04-15 16:14:49 +0530

    Time: 1 ms (63.31%) | Memory: 43 MB (93.14%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int n=strs.length;
+        Arrays.sort(strs);
+        String first=strs[0];
+        String last=strs[n-1];
+        int i=0;
+        while(i<first.length() && i<last.length()){
+            if(first.charAt(i)!=last.charAt(i)){
+                break;
+            }
+            i++;
+        }
+        return first.substring(0,i);
+        
+    }
+}
