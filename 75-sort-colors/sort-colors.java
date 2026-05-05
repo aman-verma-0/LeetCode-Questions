af8db5ff6b971ae080881e37dd52745ea34c6fc5
aman-verma-0 <vaman7256@gmail.com>
2026-05-05 09:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (59.89%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        int i=0,j=0;
+        int k=n-1;
+        while(j<=k){
+            if(nums[j]==2){
+                int temp=nums[j];
+                nums[j]=nums[k];
+                nums[k]=temp;
+                k--;
+            }
+            else if(nums[j]==0){
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        
+    }
+}
