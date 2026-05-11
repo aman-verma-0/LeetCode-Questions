cb2a0f49d5f20e75597cbce46239497e9154145f
aman-verma-0 <vaman7256@gmail.com>
2026-05-12 00:59:00 +0530

    Time: 19 ms (36.67%) | Memory: 46.9 MB (28.22%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,26 +1,24 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        int dp[][]=new int[n+1][amount+1];
-        for(int rows[]:dp){
-            Arrays.fill(rows,-1);
+        int [][]dp=new int[n+1][amount+1];
+        for(int j=1;j<=amount;j++){
+            dp[n][j]=Integer.MAX_VALUE;
         }
-        int ans=minDenominations(0,amount,coins,dp);
-        return ans==Integer.MAX_VALUE?-1:ans;
-        
-    }
-    public int minDenominations(int i,int amount,int[]coins,int[][]dp){
-        if(amount==0) return 0;
-        if(i==coins.length) return Integer.MAX_VALUE;
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        int pick=Integer.MAX_VALUE;
-        if(coins[i]<=amount){
-            int func=minDenominations(i,amount-coins[i],coins,dp);
-            if(func!=Integer.MAX_VALUE){
-                pick=1+func;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=amount;j++){
+                int pick=Integer.MAX_VALUE;
+                if(coins[i]<=j){
+                    int next=dp[i][j-coins[i]];
+                    if(next!=Integer.MAX_VALUE){
+                        pick=1+next;
+                    }
+                }
+                int skip=dp[i+1][j];
+                dp[i][j]=Math.min(pick,skip);
             }
         }
-        int skip=minDenominations(i+1,amount,coins,dp);
-        return dp[i][amount]=Math.min(pick,skip);
+        return dp[0][amount]==Integer.MAX_VALUE?-1:dp[0][amount];
+        
     }
 }
