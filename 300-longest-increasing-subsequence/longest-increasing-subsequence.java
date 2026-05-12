dedea133e3c04392e82c0f9e25a522158fa35a1e
aman-verma-0 <vaman7256@gmail.com>
2026-05-12 09:47:35 +0530

    Time: 352 ms (13.86%) | Memory: 237.2 MB (9.27%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,40 +1,24 @@
 class Solution {
-    static{
-        Runtime.getRuntime().addShutdownHook(
-            new Thread(()->{
-                try(FileWriter f=new FileWriter("display_runtime.txt")){
-                    f.write('0');
-                }catch(Exception e){}
-            })
-        );
-    }
-    private int func(int i, int prevInd, int[] arr,int[][]dp) {
-
-        if(i == arr.length) {
-            return 0;
-        }
-        if(dp[i][prevInd+1]!=-1) return dp[i][prevInd+1];
-
-        int notTake = func(i + 1, prevInd, arr,dp);
-        
-        int take = 0;
-
-        if(prevInd == -1)
-            take = func(i + 1, i, arr,dp) + 1;
-
-        else if(arr[i] >arr[prevInd])
-            take = func(i + 1, i, arr,dp) + 1;
-
-        return dp[i][prevInd+1]=Math.max(take, notTake);
-    }
-
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int[][]dp=new int[n+1][n+1];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+        int [][]dp=new int [n+1][n+1];
+        for(int rows[]:dp){
+            Arrays.fill(rows,-1);
         }
-        return func(0, -1, nums,dp);
+        return LIS(0,-1,nums,dp);
         
+    }
+    public int LIS(int i,int prevIdx,int[]nums,int[][]dp){
+        if(i==nums.length) return 0;
+        if(dp[i][prevIdx+1]!=-1) return dp[i][prevIdx+1];
+        int take=0;
+        if(prevIdx==-1){
+            take=1+LIS(i+1,i,nums,dp);
+        }
+        else if(nums[prevIdx]<nums[i]){
+            take=1+LIS(i+1,i,nums,dp);
+        }
+        int skip=LIS(i+1,prevIdx,nums,dp);
+        return dp[i][prevIdx+1]=Math.max(take,skip);
     }
 }
