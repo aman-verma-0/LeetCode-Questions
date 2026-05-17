79ba482d1388d67fda2438bfee1e5a1f5fda9078
aman-verma-0 <vaman7256@gmail.com>
2026-05-17 17:34:44 +0530

    Time: 2 ms (32.74%) | Memory: 47.3 MB (37.39%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,25 +1,17 @@
 class Solution {
     public int jump(int[] nums) {
         int n=nums.length;
-        int[]dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(0,nums,dp);   
-    }
-    public int solve(int i,int[]nums,int[]dp){
-        if(i==nums.length-1) return 0;
-        if(i>=nums.length) return Integer.MAX_VALUE;
-        if(dp[i]!=-1) return dp[i];
-        int ans=Integer.MAX_VALUE;
-        for(int j=1;j<=nums[i];j++){
-            int next=solve(i+j,nums,dp);
-            if(next!=Integer.MAX_VALUE){
-                int pick=1+next;
-                ans=Math.min(ans,pick);
+        int minJump=0;
+        int end=0;
+        int farthest=0;
+        for(int i=0;i<n-1;i++){
+            farthest=Math.max(farthest,i+nums[i]);
+            if(i==end){
+                minJump++;
+                end=farthest;
             }
-
         }
-        return dp[i]=ans;
-
-
+        return minJump;
+        
     }
 }
