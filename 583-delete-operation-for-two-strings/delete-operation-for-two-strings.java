0aa3dcd1812678143bbe718b1a92c42e2c021ec8
aman-verma-0 <vaman7256@gmail.com>
2026-05-21 16:53:21 +0530

    Time: 8 ms (20.69%) | Memory: 46.4 MB (65.96%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -3,22 +3,28 @@ class Solution {
         int n1=word1.length();
         int n2=word2.length();
         int[][]dp=new int[n1+1][n2+1];
+        for(int i=0;i<n1;i++){
+            dp[i][n2]=n1-i;
+        }
+        for(int j=0;j<n2;j++){
+            dp[n1][j]=n2-j;
+        }
         for(int i=n1-1;i>=0;i--){
             for(int j=n2-1;j>=0;j--){
-                int pick=0;
-                int skip=0;
+                int pick=Integer.MAX_VALUE;
+                int pick1=Integer.MAX_VALUE;
+                int pick2=Integer.MAX_VALUE;
                 if(word1.charAt(i)==word2.charAt(j)){
-                    pick=1+dp[i+1][j+1];
-                }else{
-                    skip=Math.max(dp[i+1][j],dp[i][j+1]);
+                    pick=dp[i+1][j+1];
                 }
-                dp[i][j]=Math.max(pick,skip);
+                else{
+                    pick1=1+dp[i+1][j];
+                    pick2=1+dp[i][j+1];
+                }
+                dp[i][j]=Math.min(pick,Math.min(pick1,pick2));
+                
             }
         }
-        int lcs=dp[0][0];
-        int l1=n1-lcs;
-        int l2=n2-lcs;
-        return l1+l2;
-        
+        return dp[0][0];
     }
 }
