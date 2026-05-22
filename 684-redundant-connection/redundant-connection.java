a575d31eeca157deae2a552733b9c41929d45a96
aman-verma-0 <vaman7256@gmail.com>
2026-05-22 10:46:35 +0530

    Time: 1 ms (91.66%) | Memory: 44.9 MB (67.06%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,34 +1,35 @@
 class Solution {
     class DSU{
-        int[]parent;
+        int[]par;
         DSU(int n){
-            parent=new int[n+1];
+            par=new int[n+1];
             for(int i=1;i<=n;i++){
-                parent[i]=i;
+                par[i]=i;
             }
         }
         public void union(int a,int b){
             int pa=find(a);
             int pb=find(b);
             if(pa!=pb){
-                parent[pb]=pa;
+                par[pb]=pa;
             }
         }
         public int find(int node){
-            if(parent[node]==node) return node;
-            return parent[node]=find(parent[node]);
+            if(par[node]==node) return node;
+            return par[node]=find(par[node]);
         }
     }
     public int[] findRedundantConnection(int[][] edges) {
         int n=edges.length;
         DSU obj=new DSU(n);
         for(int i=0;i<n;i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            if(obj.find(u)==obj.find(v)) return new int[]{u,v};
-            else obj.union(u,v);
+            int nodeA=edges[i][0];
+            int nodeB=edges[i][1];
+            int parA=obj.find(nodeA);
+            int parB=obj.find(nodeB);
+            if(parA==parB) return new int[]{nodeA,nodeB};
+            else obj.union(nodeA,nodeB);
         }
         return new int[]{-1,-1};
-        
     }
 }
