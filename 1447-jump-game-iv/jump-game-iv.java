c825870e7712afb50e8b6471603c25bdeb9a8e58
aman-verma-0 <vaman7256@gmail.com>
2026-05-29 11:35:09 +0530

    Time: 68 ms (25.79%) | Memory: 82.8 MB (35.83%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.java
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -1,66 +1,48 @@
 class Solution {
     public int minJumps(int[] arr) {
         int n=arr.length;
-        if(n==1) return 0;
-        if(n==2) return 1;
-        // List<List<Integer>>adj=new ArrayList<>();
-        // for(int i=0;i<n;i++){
-        //     adj.add(new ArrayList<>());
-        // }
-        Map<Integer,List<Integer>> map=new HashMap<>();
+        Map<Integer,List<Integer>>map=new HashMap<>();
         for(int i=0;i<n;i++){
-            if(!map.containsKey(arr[i])){
-                map.put(arr[i],new ArrayList<>());
+            int num=arr[i];
+            if(!map.containsKey(num)){
+                map.put(num,new ArrayList<>());
+                
             }
-            map.get(arr[i]).add(i);
+            map.get(num).add(i);
         }
-        // for(int i=0;i<n;i++){
-        //     if(i-1>=0) adj.get(i).add(i-1);
-        //     if(i+1<n) adj.get(i).add(i+1);
-        //     for(int idx:map.get(arr[i])){
-        //         if(idx!=i) adj.get(i).add(idx);
-        //     }
-        // }
-        boolean []vis=new boolean[n];
-        vis[0]=true;
-        Queue<Integer>q=new LinkedList<>();
+        boolean[]vis=new boolean[n];
+        vis[0]=false;
+        Queue<Integer> q=new LinkedList<>();
         q.add(0);
-        int steps=0;
-        while(!q.isEmpty()) {
-            int size = q.size();
-
-            while(size-- > 0) {
-                int cur = q.poll();
-
-                if(cur == n - 1) return steps;
-
-                // i - 1
-                if(cur - 1 >= 0 && !vis[cur - 1]) {
-                    vis[cur - 1] = true;
-                    q.add(cur - 1);
+        int step=0;
+        while(!q.isEmpty()){
+            int size=q.size();
+            
+            while(size-- >0){
+                int curIdx=q.poll();
+                if(curIdx==n-1) return step;
+                if(curIdx-1>=0 && !vis[curIdx-1]){
+                    vis[curIdx-1]=true;
+                    q.add(curIdx-1);
                 }
-
-                // i + 1
-                if(cur + 1 < n && !vis[cur + 1]) {
-                    vis[cur + 1] = true;
-                    q.add(cur + 1);
+                if(curIdx+1<n && !vis[curIdx+1]){
+                    vis[curIdx+1]=true;
+                    q.add(curIdx+1);
                 }
-
-                // same value
-                if(map.containsKey(arr[cur])) {
-                    for(int idx : map.get(arr[cur])) {
-                        if(!vis[idx]) {
-                            vis[idx] = true;
+                if(map.containsKey(arr[curIdx])){
+                    int s=map.get(arr[curIdx]).size();
+                    for(int i=0;i<s;i++){
+                        int idx=map.get(arr[curIdx]).get(i);
+                        if(idx!=curIdx && !vis[idx]){
+                            vis[idx]=true;
                             q.add(idx);
                         }
-                    }
-                    map.remove(arr[cur]); // 🔥 critical optimization
+                    }map.remove(arr[curIdx]);
                 }
             }
-            steps++;
+            step++;
         }
         return -1;
-        
 
         
     }
